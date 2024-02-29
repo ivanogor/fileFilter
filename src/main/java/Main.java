de53608aed76f525973b4f79d3448e169301d61e
ivanogor <ivanogor23@gmail.com>
2024-02-29 19:45:36 +0800

    initial commit.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.regex.Pattern;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,31 +9,25 @@ public class Main {
         final File OUTPUT_FILE_FOR_INTEGERS = Path.of("src", "out", "integers.txt").toFile();
         final File OUTPUT_FILE_FOR_STRINGS = Path.of("src", "out", "strings.txt").toFile();
 
-        final Pattern FLOAT_PATTERN = Pattern.compile("^[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?$");
-        final Pattern INTEGER_PATTERN = Pattern.compile("^[-+]?\\d+$");
 
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(INPUT_FILE));
+        try (Scanner scanner = new Scanner(INPUT_FILE);
              BufferedWriter floatsWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE_FOR_FLOATS));
              BufferedWriter integersWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE_FOR_INTEGERS));
              BufferedWriter stringsWriter = new BufferedWriter(new FileWriter(OUTPUT_FILE_FOR_STRINGS))) {
-            List<String> lines = reader.lines().toList();
-            for (String line : lines) {
-                if(FLOAT_PATTERN.matcher(line).matches()) {
-                    floatsWriter.write(line);
+
+            while (scanner.hasNext()) {
+                if(scanner.hasNextFloat()) {
+                    floatsWriter.write(Float.toString(scanner.nextFloat()));
                     floatsWriter.newLine();
+                } else if (scanner.hasNextInt()) {
+                    integersWriter.write(Integer.toString(scanner.nextInt()));
                 }
-
-                else if(INTEGER_PATTERN.matcher(line).matches()) {
-                    integersWriter.write(line);
-                    integersWriter.newLine();
-                }
-
                 else {
-                    stringsWriter.write(line);
+                    stringsWriter.write(scanner.next());
                     stringsWriter.newLine();
                 }
             }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
